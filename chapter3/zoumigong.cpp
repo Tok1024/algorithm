042a9d482e2f57bee4cb509f12d6ddf954584006
toki <3187142376@qq.com>
2026-04-14 19:55:33 +0800

    优化bfs

--- chapter3/zoumigong.cpp
+++ chapter3/zoumigong.cpp
@@ -13,24 +13,55 @@ bool check(int x, int y){
     return x >= 0 && x < n && y >= 0 && y < m && g[x][y] == 0;
 }
 
+// int bfs(){
+//     // 起点
+//     q.push({0, 0});
+    // memset(d, -1, sizeof d);
+    // d[0][0] = 0; // 初始化为-1, 防止将起点距离为0以为是未访问
+//     // 搜索
+//     while(!q.empty()){
+//         PII point = q.front();
+//         q.pop();
+//         for(int i = 0; i < 4; i++){
+//             int x = point.first + dx[i];
+//             int y = point.second + dy[i];
+//             if(check(x, y) && d[x][y] == -1){ // 必须要求以前未访问，否则会无限循环！
+//                 d[x][y] = d[point.first][point.second] + 1;
+//                 q.push({x, y});
+//             }
+//         }
+//     }
+//     return d[n-1][m-1];
+// }
+
+// 法2:每次弹出所有点（可拓展性差一点）
 int bfs(){
     // 起点
     q.push({0, 0});
+    int dist = 0;
+    // 初始化d
+    memset(d, -1, sizeof d);
+    d[0][0] = 0; // 初始化为-1, 防止将起点距离为0以为是未访问
     // 搜索
     while(!q.empty()){
-        PII point = q.front();
-        q.pop();
-        for(int i = 0; i < 4; i++){
-            int x = point.first + dx[i];
-            int y = point.second + dy[i];
-            if(check(x, y) && d[x][y] == 0){ // 必须要求以前未访问，否则会无限循环！
-                d[x][y] = d[point.first][point.second] + 1;
-                q.push({x, y});
+        int len = q.size();
+        dist++; // 每次遍历一层，dist++
+        for(int i = 0; i < len; i++){
+            PII point = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int x = point.first + dx[i];
+                int y = point.second + dy[i];
+                if(check(x, y) && d[x][y] == -1){ // 必须要求以前未访问，否则会无限循环！
+                    d[x][y] = dist; // 不好知道从哪个点来的
+                    q.push({x, y});
+                }
             }
         }
     }
     return d[n-1][m-1];
 }
+
 
 int main(){
     scanf("%d%d", &n, &m);
